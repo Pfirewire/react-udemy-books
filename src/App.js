e32f7f0754f888381f5b763ec5865c06d4fccd09
Stephen Pflug <stephen@pflug.us>
2023-02-07 09:01:13 -0600

    Generating Random ID's

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ function App() {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
-        console.log(`Need to add book with title of ${title}`);
+        // console.log(`Need to add book with title of ${title}`);
+        const updatedBooks = [
+            ...books,
+            {
+                id: randomId(),
+                title
+            }
+        ]
+        setBooks(updatedBooks);
+    };
+
+    const randomId = () => {
+        return Math.round(Math.random() * 99999);
     };
 
     const editBook = () => {
