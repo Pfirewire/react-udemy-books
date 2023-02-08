81379db97e1a2406a979e00cfdd26ccf7dc1c823
Stephen Pflug <stephen@pflug.us>
2023-02-08 06:18:51 -0600

    Refactoring the App

--- src/App.js
+++ src/App.js
@@ -1,57 +1,20 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useEffect, useContext } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import BooksContext from "./context/books";
 
 function App() {
-    const [books, setBooks] = useState([]);
-
-    const getBooks = async () => {
-        let response = await axios.get('http://localhost:3001/books');
-        setBooks(response.data);
-    };
+    const { getBooks } = useContext(BooksContext);
 
     useEffect(() => {
         getBooks();
     }, []);
 
-    const createBook = async (title) => {
-        let response = await axios.post('http://localhost:3001/books', {
-            title
-        });
-        const updatedBooks = [
-            ...books,
-            response.data
-        ]
-        setBooks(updatedBooks);
-    };
-
-    const editBookById = async (id, title) => {
-        let response = await axios.put(`http://localhost:3001/books/${id}`, {
-            title
-        });
-        setBooks(books.map((book) => {
-            if(book.id === id) {
-                return {...book, ...response.data};
-            }
-            return book;
-        }));
-    };
-
-    const deleteBookById = async (id) => {
-        let response = await axios.delete(`http://localhost:3001/books/${id}`);
-        setBooks(books.filter(book => book.id !== id));
-    };
-
-    const randomId = () => {
-        return Math.round(Math.random() * 99999);
-    };
-
     return(
         <div className="app">
             <h1>Reading List</h1>
-            <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
-            <BookCreate onCreate={createBook} />
+            <BookList />
+            <BookCreate />
         </div>
     );
 }
--- src/context/books.js
+++ src/context/books.js
@@ -1,19 +1,45 @@
 import { createContext, useState } from "react";
+import axios from "axios";
 
 const BooksContext = createContext();
 
 function Provider({ children }) {
-    const [count, setCount] = useState(5);
+    const [books, setBooks] = useState([]);
 
     const valueToShare = {
-        count,
-        incrementCount: () => {
-            setCount(count + 1);
-        }
+        books,
+        getBooks: async () => {
+            let response = await axios.get('http://localhost:3001/books');
+            setBooks(response.data);
+        },
+        createBook: async (title) => {
+            let response = await axios.post('http://localhost:3001/books', {
+                title
+            });
+            const updatedBooks = [
+                ...books,
+                response.data
+            ]
+            setBooks(updatedBooks);
+        },
+        editBookById: async (id, title) => {
+            let response = await axios.put(`http://localhost:3001/books/${id}`, {
+                title
+            });
+            setBooks(books.map((book) => {
+                if(book.id === id) {
+                    return {...book, ...response.data};
+                }
+                return book;
+            }));
+        },
+        randomId: () => {
+            return Math.round(Math.random() * 99999);
+        },
     };
 
     return (
-        <BooksContext.Provider value={valueToShare}>
+        <BooksContext.Provider value={{}}>
             {children}
         </BooksContext.Provider>
     );
